b9303d2eb977e21b667676bb8db7158aef641388
Chris Loew <cloew123@gmail.com>
2015-07-09 13:21:07 -0600

    Added helper project to run NYtram from C++

---
+++ src/cpp/runner/NytramRunner/NytramRunner.cpp
@@ -0,0 +1,27 @@
+// NytramRunner.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Nytram.h"
+
+using namespace std;
+
+void loopCallback()
+{
+	return;
+}
+void mouseCallback(int button, int pressed)
+{
+	cout << "Button: " << button << " was pressed: " << pressed << endl;
+	return;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	GameLoop_Callback myLoopCallback = loopCallback;
+	Window_SetTitle("My Blank Title");
+	Mouse_SetButtonCallback((MouseButton_Callback) mouseCallback);
+	Nytram_Run(myLoopCallback);
+	system("PAUSE");
+	return 0;
+}
